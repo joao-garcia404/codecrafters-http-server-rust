7c0dfa02511dae72aa0ee595ae8b6caafc0f0a22
João Vitor Garcia <jvdasilvaeirasgarcia@gmail.com>
2024-09-15 14:50:27 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::Write;
+use std::io::{BufRead, BufReader};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -11,8 +12,15 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
+                let buf_reader = BufReader::new(&mut stream);
+                let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                let response = match request_line.as_str() {
+                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                };
+
+                stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
