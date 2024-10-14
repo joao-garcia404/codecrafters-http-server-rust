383f8f54863b74824beef32e92ef0a98d35dfc1a
João Vitor Garcia <jvdasilvaeirasgarcia@gmail.com>
2024-10-14 08:40:26 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,31 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
+
                 let buf_reader = BufReader::new(&mut stream);
                 let request_line = buf_reader.lines().next().unwrap().unwrap();
 
                 let response = match request_line.as_str() {
-                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
-                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
+                    _ if request_line.starts_with("GET /echo/") => {
+                        println!("echo request_line: {}", request_line);
+                        let path = request_line.split(" ").nth(1).unwrap();
+
+                        println!("path: {}", path);
+
+                        let param = path.split("/").last().unwrap();
+                        let param_len = param.len();
+
+                        println!("param_len: {}", param_len);
+                        println!("param: {}", param);
+
+                        format!(
+                            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                            param_len,
+                            param,
+                        )
+                    }
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
                 };
 
                 stream.write_all(response.as_bytes()).unwrap();
